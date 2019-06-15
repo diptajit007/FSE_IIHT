7ab2c2d399d9e4a2a6ca2d23e28368289d199ffc
diptajit007 <diptajit007@gmail.com>
2019-06-15 09:08:04 -0700

    Base code for API UI

---
+++ API/FSE_IIHT_SBA/Models/ParentTask.cs
@@ -0,0 +1,11 @@
+﻿ 
+
+namespace FSE_SBA_PM.Models
+{
+    public class ParentTask
+    {
+        public int ParentTaskId { get; set; }
+
+        public string ParentTaskName { get; set; }
+    }
+}
